06e7a12588ef06cb6753b864e08af329983bc272
Jiao Lin <jiao.lin@gmail.com>
2021-01-24 23:03:13 -0500

    added command to get access to aws ecr:   build_and_update_ecr.sh

--- awsbatch/build_and_update_ecr.sh
+++ awsbatch/build_and_update_ecr.sh
@@ -1,3 +1,4 @@
 docker build -t mcvine/awsbatch_fetch_and_run .
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/y9i1c6r0
 docker tag mcvine/awsbatch_fetch_and_run:latest public.ecr.aws/y9i1c6r0/mcvine/awsbatch_fetch_and_run:latest
 docker push public.ecr.aws/y9i1c6r0/mcvine/awsbatch_fetch_and_run:latest
